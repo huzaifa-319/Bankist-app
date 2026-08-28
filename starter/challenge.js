9944a27a1ee5ac8e4f1dd592df407f3d85d79c05
Huzaifa Ahmad <kentomomota758@gmail.com>
2026-08-28 23:10:59 +0500

    Deposit and withdraul movements added

---
+++ starter/challenge.js
@@ -0,0 +1,28 @@
+'use strict';
+/*
+const julisData = [9, 16, 6, 8, 3];
+const katesData = [4, 1, 15, 8, 3];
+
+const checkDogs = function (dogsJulias, dogsKate) {
+    console.log('=====Julias Dogs=====');
+    dogsJulias.slice(1, 3).forEach(function (val, i, arr) {
+        if (val >= 3) {
+            console.log(`Dog Number ${i + 1} is and Adult and is ${val} Years old`);
+        } else {
+            console.log(`Dog Number ${i + 1} puppy and is still a puppy`);
+    }
+  });
+
+  console.log('=====kate dogs=====');
+  dogsKate.forEach(function (val, i, arr) {
+    if (val >= 3) {
+        console.log(`Dog Number ${i + 1} is and Adult and is ${val} Years old`);
+    } else if (val < 3) {
+        console.log(`Dog Number ${i + 1} puppy and is still a puppy`);
+    }
+});
+};
+
+checkDogs(julisData, katesData);
+
+*/
